32943e1a9e022177b93f7f1a31c49ed39a4a34c4
Niko Kivelä <niko.kivela@verkkokauppa.com>
2014-08-27 20:13:12 +0300

    Add bin/ and install/update script within it

---
+++ bin/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd $HOME
+
+for f in autoload bundle colors plugin; do
+    ln -s ~/.vim-git/$f ~/.vim/ 2> /dev/null || true
+done
+
+cd .vim-git/bundle
+
+for r in guns/vim-clojure-highlight tpope/vim-fireplace tpope/vim-classpath guns/vim-clojure-static kien/rainbow_parentheses.vim vim-scripts/LustyExplorer vim-scripts/LustyJuggler vim-scripts/LustyJuggler; do
+    d=`echo $r | sed 's|^.*/||'`
+    echo "[$r]"
+    test -d $d && pushd $d > /dev/null && git pull &&  popd > /dev/null
+    ! test -d $d &&  git clone git@github.com:$r.git
+done
+
